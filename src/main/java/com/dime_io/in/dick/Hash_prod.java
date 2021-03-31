e6a03c794a308b70b6972677f2d25c93ba3fb210
otec1 <tobeokoko18@gmail.com>
2021-03-31 09:29:45 +0100

    v1

---
+++ src/main/java/com/dime_io/in/dick/Hash_prod.java
@@ -0,0 +1,47 @@
+package com.dime_io.in.dick;
+
+public class Hash_prod {
+	
+	
+	
+	//DEV
+    /*static 	String url="jdbc:mariadb://localhost/walexfab_blog", //walexfab_blogs
+    			username="root",
+    			password="7h8g2cc",
+    			driver="org.mariadb.jdbc.Driver";*/
+
+
+
+    
+    
+    //PROD
+     static String url="jdbc:mariadb://localhost/walexfab_dims_server",
+  		username="walexfab_dims_serverO",
+		password="blackmagic54321",
+		driver="org.mariadb.jdbc.Driver"; 
+    
+	
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
